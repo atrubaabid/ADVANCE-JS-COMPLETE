63a6edfeecd5e51a14651eb2862a2969fe50b45a
atrubaabid <atrubaabid7@gmail.com>
2026-05-25 15:54:15 +0500

    in p

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 // document.write("Hello World")
 
 
+
 //VARIABLES
 
 // var n = 50      
@@ -12,13 +13,69 @@
 // const o = 50
 
 
+
+
 //TERNARY OPERATOR
 
-let v = 5;
-let d = "string";
-let a = true;
-console.log(`Hello World ${v + ' ' + d + ' ' + a}`);
-document.write(`<h1> Hello World ${v + ' ' + d + ' ' + a} <h1/>`);
+// let v = 5;
+// let d = "string";
+// let a = true;
+// console.log(`Hello World ${v + ' ' + d + ' ' + a}`);
+// document.write(`<h1> Hello World ${v + ' ' + d + ' ' + a} <h1/>`);
+// console.log(`
+//     1 MY
+//     2 NAME 
+//     3 IS
+//     4 ATRUBA`);
+
+
+
+
+// DATA TYPES
+
+//1. PRIMITIVE DATA TYPES
+
+// let a = 10;                      //number
+// let b = "Atruba"                   //string
+// let c = true;                     //boolean
+// let d;                              //undefined
+// let e = null;                     //object       types of object
+// let f=Symbol("qweerty");           //symbol
+
+// console.log(a, typeof (a));
+// console.log(b, typeof (b));
+// console.log(c, typeof (c));
+// console.log(d, typeof (d));
+// console.log(e, typeof (e));
+// console.log(f, typeof (f));
+
+
+
+//2. NON-PRIMITIVE DATA TYPES
+
+// let g = [10, 20, 30, 40, 50];               //Array       types of object
+// console.log(g, typeof (g));
+
+
+// let obj = {                                  //Object       types of object
+//     name: 'Atruba',
+//     class: 'Graduation'
+// }
+// console.log(obj, typeof (obj));
+
+
+
+// function showdata() {                          //Function     
+
+// }
+
+// console.log(showdata, typeof (showdata));
+
+
+
+
+
+
 
 
 
