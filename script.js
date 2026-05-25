1a8473a96b760b43119c69bdf2d0fccde9cbc267
atrubaabid <atrubaabid7@gmail.com>
2026-05-25 14:57:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// BASICS
+
+// alert("Hello World");
+// console.log("Hello World");
+// document.write("Hello World")
+
+
+//VARIABLES
+
+// var n = 50      
+// let m = 50
+// const o = 50
+
+
+//TERNARY OPERATOR
+
+let v = 5;
+let d = "string";
+let a = true;
+console.log(`Hello World ${v + ' ' + d + ' ' + a}`);
+document.write(`<h1> Hello World ${v + ' ' + d + ' ' + a} <h1/>`);
+
+
+
