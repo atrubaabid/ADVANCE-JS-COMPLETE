e8712f3582d917fd7678d593f4c762fb1a242e03
atrubaabid <atrubaabid7@gmail.com>
2026-05-26 19:16:19 +0500

    Done

---
+++ array&Methods.js
@@ -0,0 +1,165 @@
+//ARRAY
+
+// let user = [10, 20, 30, 40, 50, 60, 70,3,5,7,9];
+// console.log(user);
+// console.log(user[0]);
+// console.log(user[6]);
+// console.log(user[user.length-3]);
+
+
+//NESTED ARRAY
+// console.log(user[7][2]);
+
+//FOR LOOP
+
+// for (i = 0; i < user.length; i++) {
+//     console.log(user[i]);
+
+// }
+
+// console.log("");
+// console.log("");
+
+
+
+// for (i = user.length - 1; i > 0; i--) {
+//     console.log(user[i]);
+
+// }
+
+
+//FOR OF LOOP
+
+// for (let v of user) {
+//     console.log(v);
+
+// }
+
+//FOR IN LOOP
+
+// for (let v in user) {
+//     console.log(v, user[v]);
+
+// }
+
+
+//SUM PROGRAM
+
+// let sum = 0;
+// for (let v of user) {
+//     console.log(v);
+//     sum+=v
+
+// }
+
+// console.log(sum);
+
+
+//EVEN NUMBER PROGRAM
+
+// for(let v of user){
+//     if(v%2===0){
+//         console.log(v);
+
+//     }
+// }
+
+
+//UPDATE ARRAY
+
+// user[1]= 222;
+// console.log(user);
+
+
+//ARRAY METHODS WHO CHANGE ORIGINAL ARRAY
+
+// let a = [10, 20, 30, 40, 50, 60, 70, 3, 5, 7, 9];
+// a.push(77)              //add in last
+// a.push(11)
+
+// let b = a.pop()          //remove in last
+// console.log(b);
+
+// a.pop()
+// console.log(a);
+
+
+// a.shift()                   //remove start
+// console.log(a);
+
+// a.unshift(1234)             //add start
+// console.log(a);
+
+
+// a.splice(3,1,);    //delete in splice
+
+// a.splice(1, 0, 4000)   //add in splice
+
+// a.splice(0, 1, 333)    //replace in splice
+// console.log(a);
+
+// let a = [20, 50, 80, 10, 40];
+
+// a.sort();                   //sort alphabetically like a-z and 0-10
+// console.log(a);
+
+// a.reverse()        //reverse all the condition
+// console.log(a);
+
+
+//your value, start position, end poistion 
+
+
+// a.fill(9, 2, 3)          //change value
+// console.log(a);
+
+
+//LOGICAL QUESTION
+
+
+// jb ham array ko loop me remove ker rahy hoty hyn tw end me 2 elements bach jaty hyn or choty array me loop nhi chlta
+
+
+// let a = [20, 50, 80, 10, 40];
+// for (let g of a) {
+//     a.pop();
+//     console.log(a);
+
+// }
+
+
+// =====================================================================
+
+// let r=[1,2,3,4];
+// for(let v of r){
+//     r[3]=v
+// }
+// console.log(r);
+
+// =====================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
