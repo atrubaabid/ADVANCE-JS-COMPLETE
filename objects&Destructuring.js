bedfe2a83c3d93d5ca234abbddd6642b4a7e5184
atrubaabid <atrubaabid7@gmail.com>
2026-06-01 06:46:46 -0700

    Done

---
+++ objects&Destructuring.js
@@ -0,0 +1,105 @@
+// let user = {
+//     name: "Atruba",
+//     email: "atruba@gmail.com",
+//     isStudent: true,
+//     123: "LL"
+// }
+
+
+// console.log(user);
+// console.log(user.name);
+// console.log(user.email);
+// console.log(user.isStudent);
+// console.log(user['123']);  
+
+
+
+//ADD ANOTHER KEY IN OBJECT
+// user.age = 22
+// console.log(user);
+
+
+
+//UPDATE ANOTHER KEY IN OBJECT
+// user.email = "zzzzzzzzz"
+// console.log(user);
+
+
+
+//DELETE ANOTHER KEY IN OBJECT
+// delete user.email
+// console.log(user);
+
+// =================================================
+
+//WE CAN ALSO ADD KEY LIKE THIS
+
+// let age = 22;
+// let user = {
+//     name: "Atruba",
+//     email: "atruba@gmail.com",
+//     isStudent: true,
+//     123: "LL",
+//     age
+
+// }
+// console.log(user);
+
+
+
+// =================================================
+
+// DESTRUCTURING
+
+// let user = {
+//     name: "Atruba",
+//     email: "atruba@gmail.com",
+//     isStudent: true,
+//     123: "LL",
+
+// }
+
+// let { name, email } = user;
+//NOW WE CAN USE THIS LIKE VARIABLE
+// console.log(name, email);
+// console.log(email);
+// console.log(name);
+
+
+// RENAME THE KEY
+// let { name: Fullname } = user
+// console.log(Fullname);
+
+// =================================================
+
+//REST OPERATOR
+
+
+// let user = {
+//     name: "Atruba",
+//     email: "atruba@gmail.com",
+//     isStudent: true,
+//     123: "LL",
+// }
+
+// let { name, ...otherdetails } = user
+// console.log(name);
+// console.log(otherdetails);
+// console.log(user);
+
+
+// =================================================
+
+// NESTED DESTRUCTURING
+
+
+
+
+
+
+
+
+
+
+
+
