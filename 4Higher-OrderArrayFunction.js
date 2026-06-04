6939d201ec58f823e5b0ca6b65b9222e0f2d0ca0
atrubaabid <atrubaabid7@gmail.com>
2026-06-04 08:03:58 -0700

    Done

---
+++ 4Higher-OrderArrayFunction.js
@@ -0,0 +1,88 @@
+//MAP ARRAY
+//her element pr alg alg kam kerta hy
+
+// let l = [10, 20, 30, 40];
+// l.map((v, i) => {
+//     console.log(v, i);
+
+// })
+
+
+// ==============================================
+
+
+
+// let l = [10, 20, 30, 40];
+// let finalArray = l.map((v, i) => {
+//     return v + 5
+// })
+
+// console.log(finalArray);
+
+
+// ==============================================
+
+
+// let l = [10, 20, 30, 40];
+// let finalArray = l.map((v) => v + 5)
+
+// console.log(finalArray);
+
+
+// ==============================================
+
+
+// let a = [1, 2, 3];
+// a.map((v) => {
+//     console.log(v * v);
+
+// })
+
+// ==============================================
+//alag arrow function bna k usko as a call back map me pass krna
+
+// let a = [1, 2, 3, 4, 5];
+// let square = (v) => v ** v;
+// let result = a.map(square);
+// console.log(result);
+
+
+// ==============================================
+
+//FILTER ARRAY
+//new array me hamari true conditions value filter kr k deta hy
+
+
+
+// let a = [0, 3, , 0, 60, 9, 30, 77, 88];
+// a.filter((v) => {
+//     if (v % 3 === 0 && v != 0) {
+//         console.log(v);
+
+//     }
+// })
+
+
+// ==============================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
