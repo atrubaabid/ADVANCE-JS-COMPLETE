106e157eb8b35c4daa98bd88084c89f891661242
atrubaabid <atrubaabid7@gmail.com>
2026-06-05 06:15:31 -0700

    Done

---
+++ 5DOM.js
@@ -0,0 +1,72 @@
+// console.log(document.body.outerHTML);
+// console.log(document.head.outerHTML);
+// console.log(document);
+// console.log(window.document);c
+
+// ==============================================
+// SELECT ELEMENT BY ID
+
+// let h1 = document.getElementById("heading");
+// console.log(h1.outerHTML);
+// console.log(h1.innerHTML);
+// console.log(h1.innerText);
+
+// let p = document.getElementById("para")
+// console.log(p.outerHTML);
+
+// ==============================================
+
+//QUERY SELECTOR
+
+//BY ID
+
+// let inputName = document.querySelector("#name")
+// console.log(inputName);
+// let inputEmail = document.querySelector("#email")
+// console.log(inputEmail);
+
+//BY CLASS
+
+// let inputName = document.querySelector(".name")
+// console.log(inputName);
+// console.log(inputName.className);
+// let inputEmail = document.querySelector(".email")
+// console.log(inputEmail);
+// console.log(inputEmail.className);
+
+
+//BY TAGNAME
+
+// let inputName = document.querySelector("input")
+// console.log(inputName);
+
+// ==============================================
+
+//ATTRIBUTES PRINT AND UPDATE
+
+// let inputEmail = document.querySelector("#email")
+// console.log(inputEmail);
+
+// //Print
+// console.log(inputEmail.type);
+// console.log(inputEmail.placeholder);
+// console.log(inputEmail.className);
+// console.log(inputEmail.id);
+
+// //update
+// inputEmail.type = "password"
+// inputEmail.placeholder = "Enter your email"
+// inputEmail.className = "Atruba"
+// inputEmail.id = "Atruba"
+// console.log("");
+
+// //final print
+// console.log(inputEmail.type);
+// console.log(inputEmail.placeholder);
+// console.log(inputEmail.className);
+// console.log(inputEmail.id);
+
+// ==============================================
+
+
+
