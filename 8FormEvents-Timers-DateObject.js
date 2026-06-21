f46e0032aad1764ee535daadb9479335a4160c69
atrubaabid <atrubaabid7@gmail.com>
2026-06-21 13:33:23 -0700

    Done

--- 8FormEvents-Timers-DateObject.js
+++ 8FormEvents-Timers-DateObject.js
@@ -1,4 +1,30 @@
 // FORM-SUBMIT
 
+// when you want to cancle some events
+
+// console.log("Atruba");
+
+// let body = document.querySelector("body");
+// body.addEventListener("contextmenu", (e) => {
+//     e.preventDefault();
+// })
+
+
+// ============================================================================
+
+// watch this file for full explanation of forms
+
+// p-Form.html
+
+
+// ============================================================================
+
+
+
+
+
+
+
+
 
 
